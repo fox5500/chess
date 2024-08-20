911e8310cd99b1794f4a40709187c31d5e6f5546
Fox5500 <ethanfox5500@gmail.com>
2024-08-20 15:35:44 -0400

    changed it to OOP

--- src/definitions/chessboard.py
+++ src/definitions/chessboard.py
@@ -6,31 +6,36 @@ start_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
 fen = start_fen
 
 
-#Fields of the Notation
-fen_fields = fen.split()
-piece_location = fen_fields[0]
-active_color = fen_fields[1]
-castling = fen_fields[2]
-enpassant = fen_fields[3]
-halfmove_clock = fen_fields[4]
-move_number = fen_fields[5]
-board = []
-#def generate_fen
+class gamestate:
+    def __init__(self,fen):
+        #parse the fen string first
+        fen_fields = fen.split()
+        self.piece_location = fen_fields[0]
+        self.active_color = fen_fields[1] # going to have to write a function to modify each section of the fen message based on player actions
+        self.castling = fen_fields[2]
+        self.enpassant = fen_fields[3]
+        self.halfmove_clock = fen_fields[4]
+        self.move_number = fen_fields[5]
+        self.board = self.parseboard(self.piece_location)
 
+    def parseboard(self, piece_location):
+        board = []
+        # define the fen notation parts and how to parse the message
+        for rank in piece_location.split('/'):
+            row = []
+            for char in rank:
+                if char.isdigit():
+                    row.extend(['.'] * int(char))
+                else:
+                    row.append(char)
+            board.append(row)
+        return board
 
-
-
-
-## Print the board function
-for rank in piece_location.split('/'):
-    row = []
-    for char in rank:
-        if char.isdigit():
-            row.extend(['.'] * int(char))
-        else:
-            row.append(char)
-    board.append(row)
-for row in board:
-    print(' '.join(row))
- 
+    def print_board(self):
+        ## Print the board function
+        for row in self.board:
+            print(' '.join(row))
     
+
+game = gamestate(fen)
+game.print_board()
